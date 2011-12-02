f3700443ba7eef0cfd9f52942ec33cccf6e5544d
Alexander Dean <dstar@csh.rit.edu>
2011-12-02 13:32:29 -0500

    First commit, directory structure and base outline

---
+++ src/SkipJack.java
@@ -0,0 +1,12 @@
+
+
+public class SkipJack {
+    public static String Encrypt( String key, String message ){
+        return "";
+    }
+
+    public static String Decrypt( String key, String message ){
+        return "";
+    }
+}
+
---
+++ tests/Test.java
@@ -0,0 +1,10 @@
+/** Simple interface for unit tests. **/
+public interface Test {
+
+    // Descriptive name for the test
+    public String name;
+
+    // Run the test
+    public boolean run();
+}
+
